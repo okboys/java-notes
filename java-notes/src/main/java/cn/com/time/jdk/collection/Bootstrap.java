87a734388ec3bda26f7d065a80a4a1680a3a601f
tong.luo <iXYfLXw3>
2017-08-15 18:29:54 +0800

    集合类

---
+++ java-notes/src/main/java/cn/com/time/jdk/collection/Bootstrap.java
@@ -0,0 +1,12 @@
+package cn.com.time.jdk.collection;
+
+/**
+ * Created by TimaNetworks on 2017/8/15.
+ */
+public class Bootstrap {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ java-notes/src/main/java/cn/com/time/jdk/collection/map/MapNotes.java
@@ -0,0 +1,15 @@
+package cn.com.time.jdk.collection.map;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by TimaNetworks on 2017/8/15.
+ */
+public class MapNotes {
+
+    public void hashMap(){
+        Map<String,String> m=new HashMap<>();
+    }
+
+}
